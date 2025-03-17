3b6b606488a19c137456a4c8198715775105eb4c
4rcanine <145784416+4rcanine@users.noreply.github.com>
2025-03-17 04:29:22 +0000

    adding initial routes

--- Finalproj/routes/web.php
+++ Finalproj/routes/web.php
@@ -5,3 +5,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::get('/register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('/enroll', 'EnrollmentController@showForm')->name('enroll');
+
+
+Route::get('/schedule', 'ScheduleController@show')->name('schedule');
+
+
+Route::get('/payment', 'PaymentController@showPaymentForm')->name('payment');
+
+
+Route::get('/grades', 'GradesController@show')->name('grades');
+
+// 
+Route::get('/check', 'CheckController@show')->name('check');
+
